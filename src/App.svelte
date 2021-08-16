57fd4b56ec28a3f5dd8f0af0d62735902b8cbea5
Daniel Apodaca <apodacaduron@gmail.com>
2021-08-15 22:22:49 -0400

    test no router

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-  import { Router, Route } from "svelte-navigator";
   import Home from "./pages/Home.svelte";
   import Nav from "./components/General/Nav.svelte";
 </script>
 
-<Router>
+<template>
   <Nav />
-  <Route path="/"><Home /></Route>
-</Router>
+  <Home />
+</template>
